37f3689bb857133cd032630fdb71b524cd4a65d0
Mark <mhill601@gmail.com>
2022-03-10 13:11:55 -0600

    Homepage improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: {
     enabled: true, //Turn on when using production
     content: ["./**/*.liquid"],
+    whitelist: [
+      'grid-cols-1',
+      'grid-cols-5'
+    ]
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
